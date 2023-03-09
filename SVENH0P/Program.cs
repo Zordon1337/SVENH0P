f0f251b26c2d2d648e4661fd583cc5f4a7481750
Zordon <65111609+Zordon1337@users.noreply.github.com>
2023-03-09 19:36:20 +0100

    Add files via upload

---
+++ SVENH0P/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Memory;
+
+namespace SVENH0P
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern bool GetAsyncKeyState(int key);
+        static void Main()
+        {
+            Mem m = new Mem();
+            Console.WriteLine("Injecting!");
+            {
+                int procid = m.GetProcIdFromName("svencoop.exe");
+                m.OpenProcess(procid);
+            }
+            
+            while(true)
+            {
+                if (GetAsyncKeyState(0x20))
+                {
+                    m.WriteMemory("client.dll+5FC244", "int", "5");
+                    m.WriteMemory("client.dll+5FC244", "int", "4");
+                }
+            }
+            
+            
+        }
+    }
+}
